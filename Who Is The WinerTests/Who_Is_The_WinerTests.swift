5dddc2876a3dabc15f613f884509a4500b4c03e4
gmehedi <mehedi.self@gmail.com>
2020-02-13 17:52:58 +0600

    Update Who is the winer

---
+++ Who Is The WinerTests/Who_Is_The_WinerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Who_Is_The_WinerTests.swift
+//  Who Is The WinerTests
+//
+//  Created by Mehedi on 2/12/20.
+//  Copyright © 2020 MatrixSolution. All rights reserved.
+//
+
+import XCTest
+@testable import Who_Is_The_Winer
+
+class Who_Is_The_WinerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
